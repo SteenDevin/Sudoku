ddbfd315ea5a425264b593fdb01ba58343290fc5
IlizaliturriAlbert <ajilizal@gmail.com>
2014-05-10 20:10:15 -0600

    Merge origin/master
    Conflicts:
    	README.md

---
+++ src/sudoku/Location.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sudoku;
+
+/**
+ *
+ * @author A.J. Ilizaliturri
+ */
+public class Location {
+    int row;
+    int column;
+    
+    public Location() {
+        
+    }
+  
+    public void occupyLocation() {
+       System.out.println(" ");
+    }
+    
+    public void displayMarker() {
+       System.out.println(" ");
+    
+   }
+}
